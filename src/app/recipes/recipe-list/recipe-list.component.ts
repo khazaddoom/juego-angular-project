d1e74f6b912f818ca29fca0c03e916d5e12ee8f2
unknown <31121986g@gmail.com>
2019-06-05 13:23:18 +0530

    adding a model to the app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe-list/recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  Recipes: Recipe[] = [
+    new Recipe('Chicken Curry', 'An indian dish made from boiled chicheck pieces.', 'https://recipesfromapantry.com/wp-content/uploads/2018/05/instant-pot-chicken-curry-11.jpg')
+  ];
 
   constructor() { }
 
