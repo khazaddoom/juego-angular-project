9e437156f51a6ef3247181ec431c45eda662de8d
unknown <31121986g@gmail.com>
2019-06-05 15:21:28 +0530

    added basic template and components with styles

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,9 +10,11 @@ import { Recipe } from '../recipe-list/recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Chicken Curry',
+    new Recipe(
+    'Chicken Curry',
     'An indian dish made from boiled chicken pieces.', 
-    'https://recipesfromapantry.com/wp-content/uploads/2018/05/instant-pot-chicken-curry-11.jpg')
+    'https://recipesfromapantry.com/wp-content/uploads/2018/05/instant-pot-chicken-curry-11.jpg'
+    )
   ];
 
   constructor() { }
