fd976ef3faf0e7c4fe70ff630b4c354ef520e19f
unknown <31121986g@gmail.com>
2019-06-08 13:30:39 +0530

    enabled dropdowns using directives

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, HostListener, ElementRef, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  @HostBinding('class.open') clicked = false;
+
+  @HostListener('click') onMouseClick() {
+    this.clicked = !this.clicked;
+  }
+
+}
